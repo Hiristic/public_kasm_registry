02b133f1082a7ebbcc2a148f878142fb4d3501a3
Hiristic <55379302+Hiristic@users.noreply.github.com>
2024-11-14 16:22:17 +0100

    Correct URLs

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Public Kasm registry',
+    description: 'Public registry for Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://hiristic.github.io/public_kasm_registry/ ',
+    contactUrl: 'https://github.com/Hiristic/public_kasm_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/public_kasm_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
